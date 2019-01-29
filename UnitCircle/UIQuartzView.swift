81e32c48a09e23b19de3faf1174563cdb898af84
Usman Rashid <usman.rashid@gmail.com>
2019-01-29 14:49:21 -0800

    Draws dashed grid lines along with axis and unit circle.

--- UnitCircle/UIQuartzView.swift
+++ UnitCircle/UIQuartzView.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
+func * (lhs: Int, rhs: CGFloat) -> CGFloat { return CGFloat (lhs) * rhs }
+func / (lhs: CGFloat, rhs: Int) -> CGFloat { return lhs / CGFloat (rhs) }
+
 class UIQuartzView: UIView {
 
+    // Each grid segment represents 0.2 units
+    let gridSegments = 12
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
@@ -29,40 +35,70 @@ class UIQuartzView: UIView {
         let origin = CGPoint (x:0.0, y:0.0)
 
         // Bottom-Right corner of the view in view coordinates
-        let diagPt =  CGPoint (x:self.frame.width, y:self.frame.height)
+        let extent =  CGPoint (x:self.frame.width, y:self.frame.height)
         
-        // Draw using the properties set in the context
+        // Draw using default properties set in CG context
         context.move(to: origin)
-        context.addLine(to: CGPoint(x:diagPt.x, y:origin.y))
-        context.addLine(to: diagPt)
-        context.addLine(to: CGPoint(x:origin.x, y:diagPt.y))
+        context.addLine(to: CGPoint(x:extent.x, y:origin.y))
+        context.addLine(to: extent)
+        context.addLine(to: CGPoint(x:origin.x, y:extent.y))
         context.addLine(to: origin)
 
         context.strokePath()
     }
     
     func drawGrid (_ context:CGContext) {
-        // Drawing Area Size
+        // Save existing graphic state
+        context.saveGState()
+        
+        // Size of Drawing Area in view coordinates
         let width = self.frame.width
         let height = self.frame.height
         
         // Origin of the drawing area
-        let origin = CGPoint(x:0.0, y:0.0)
+        let origin = CGPoint(x: 0.0, y: 0.0)
 
         // Center point of the drawing area
-        let center = CGPoint (x:self.frame.width/2.0, y:self.frame.height/2.0)
+        let center = CGPoint (x: width/2.0, y: height/2.0)
         
+        // Size of grid in view coordinates
+        let gridSize = width / gridSegments
+        
+        // Set dashed pattern for grid lines
+        context.setLineDash(phase: 0.0, lengths: [1.0, 2.0])
+        
+        // Vertical grid lines
+        for i in 0...gridSegments {
+            context.move(to: CGPoint(x: i*gridSize, y: origin.y))
+            context.addLine(to: CGPoint(x: i*gridSize, y: height))
+        }
+        context.strokePath()
+        
+        // Horizontal grid lines
+        for i in 0...gridSegments {
+            context.move(to: CGPoint(x: origin.x, y: i * gridSize))
+            context.addLine(to: CGPoint(x: width, y: i * gridSize))
+        }
+        context.strokePath()
+
+        // Set solid pattern for axis
+        context.setLineDash(phase: 0.0, lengths: [])
+
         // Vertical Axis
-        context.move(to: CGPoint(x:center.x, y:origin.y))
-        context.addLine(to: CGPoint(x:center.x, y:height))
+        context.move(to: CGPoint(x: center.x, y: origin.y))
+        context.addLine(to: CGPoint(x:center.x, y: height))
 
         // Horizontal Axis
         context.move(to: CGPoint(x:origin.x, y:center.y))
         context.addLine(to: CGPoint(x:width, y:center.y))
-        
-        let gridSpacing = width / 12.0
-        context.addArc(center: center, radius: gridSpacing*5, startAngle: 0.0, endAngle: 2*CGFloat.pi, clockwise: false)
 
+        // Draw the Unit Circle
+        // Each grid segment = 0.2 units
+        // 5 grid segments = 1.0 unit
+        context.addArc(center: center, radius: gridSize * 5, startAngle: 0.0, endAngle: 2 * .pi, clockwise: false)
         context.strokePath()
+        
+        // Restore previous graphic state
+        context.restoreGState()
     }
 }
