6c33691bff7eae2ce26337556aceb97b71e27818
Usman Rashid <usman.rashid@gmail.com>
2019-01-29 17:03:07 -0800

    Adds step lines

--- UnitCircle/UIQuartzView.swift
+++ UnitCircle/UIQuartzView.swift
@@ -11,19 +11,45 @@ import UIKit
 func * (lhs: Int, rhs: CGFloat) -> CGFloat { return CGFloat (lhs) * rhs }
 func / (lhs: CGFloat, rhs: Int) -> CGFloat { return lhs / CGFloat (rhs) }
 
+func + (lhs: CGFloat, rhs: Float) -> CGFloat { return lhs + CGFloat (rhs) }
+func - (lhs: CGFloat, rhs: Float) -> CGFloat { return lhs - CGFloat (rhs) }
+
 class UIQuartzView: UIView {
 
     // Each grid segment represents 0.2 units
     let gridSegments = 12
+
+    enum axis {
+        case plus_x
+        case plus_y
+        case minus_x
+        case minus_y
+    }
+
+    enum step {
+        case pi_by_six
+        case pi_by_four
+        case pi_by_three
+    }
+    
+    let axisAngle : [axis:Float] = [
+        .plus_x:     0.0,
+        .plus_y:    .pi/2,
+        .minus_x:   .pi,
+        .minus_y:   .pi/2 * 3
+    ]
+
+    let stepAngle : [step:Float] = [
+        .pi_by_six:     .pi/6,
+        .pi_by_four:    .pi/4,
+        .pi_by_three:   .pi/3
+    ]
     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
 
         let context: CGContext = UIGraphicsGetCurrentContext()!
-        
-//        context.setLineWidth(2.0)
-//        context.setStrokeColor(red:0.333, green:0.333, blue:0.333, alpha:1.0);
 //        context.setFillColor(red:0.0, green:0.0, blue:0.0, alpha:1.0);
         
         drawBorder(context)
@@ -36,7 +62,7 @@ class UIQuartzView: UIView {
 
         // Bottom-Right corner of the view in view coordinates
         let extent =  CGPoint (x:self.frame.width, y:self.frame.height)
-        
+
         // Draw using default properties set in CG context
         context.move(to: origin)
         context.addLine(to: CGPoint(x:extent.x, y:origin.y))
@@ -65,6 +91,7 @@ class UIQuartzView: UIView {
         let gridSize = width / gridSegments
         
         // Set dashed pattern for grid lines
+        context.setLineWidth(1.0)
         context.setLineDash(phase: 0.0, lengths: [1.0, 2.0])
         
         // Vertical grid lines
@@ -81,7 +108,7 @@ class UIQuartzView: UIView {
         }
         context.strokePath()
 
-        // Set solid pattern for axis
+        // Use solid pattern for axis
         context.setLineDash(phase: 0.0, lengths: [])
 
         // Vertical Axis
@@ -97,8 +124,33 @@ class UIQuartzView: UIView {
         // 5 grid segments = 1.0 unit
         context.addArc(center: center, radius: gridSize * 5, startAngle: 0.0, endAngle: 2 * .pi, clockwise: false)
         context.strokePath()
+
+        // Draw step lines in light gray
+        context.setLineWidth(1.0)
+        context.setStrokeColor(red:0.667, green:0.667, blue:0.667, alpha:1.0);
         
+        for (_, base) in axisAngle {
+            for (_, step) in stepAngle {
+                drawLine(context, radius: Float(gridSize * 5.25), angle: base + step)
+            }
+        }
+
         // Restore previous graphic state
         context.restoreGState()
+    }
+    
+    // Draw a radial line of specified length and at a specified positive angle
+    func drawLine (_ context:CGContext, radius length:Float, angle theta:Float) {
+        // Center point of the drawing area
+        let center = CGPoint (x: self.frame.width/2.0, y: self.frame.height/2.0)
+        
+        // End point of the radial line
+        let endPt = CGPoint (x: center.x + length * cos(theta), y: center.y - length * sin(theta))
+        
+        // Draw line
+        context.move(to: center)
+        context.addLine(to: endPt)
+        
+        context.strokePath()
     }
 }
