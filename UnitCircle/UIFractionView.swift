19e8af69e7814af24fee39997b46c11df05a2478
Usman Rashid <usman.rashid@gmail.com>
2019-01-31 17:20:37 -0800

    Adds parsing function for fraction string.

--- UnitCircle/UIFractionView.swift
+++ UnitCircle/UIFractionView.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class UIFractionView: UIView {
-    var numerator: UILabel = {
+    private var numerator: UILabel = {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         label.textAlignment = .center
-//        label.backgroundColor = .red
         return label
     }()
     
-    var embar: UILabel = {
+    private var embar: UILabel = {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         label.textAlignment = .center
         label.numberOfLines = 1
@@ -25,46 +24,90 @@ class UIFractionView: UIView {
         return label
     }()
 
-    var denominator: UILabel = {
+    private var denominator: UILabel = {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         label.textAlignment = .center
         return label
     }()
 
+    // Init from Code
     override init (frame : CGRect) {
         super.init(frame : frame)
         setupView()
     }
     
+    // Init from Storyboard or Xib
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupView()
     }
     
-    func setupView() {
-        backgroundColor = .white
-        
-        // Embar in the middle
-        embar.frame = CGRect(x:0, y:0, width:self.frame.width, height:self.frame.height)
-        //        embar.frame = CGRect(x:0, y:self.frame.height/2 - 0.1 * self.frame.height, width:self.frame.width, height:self.frame.height * 0.2)
-        addSubview(embar)
-        
+    // Place UILabels for numerator, denominator and middle bar
+    private func setupView() {
+        backgroundColor = .lightGray
+
         // Top half for numerator
-        numerator.frame = CGRect(x:0, y:0, width:self.frame.width, height:self.frame.height/2)
+        numerator.frame = CGRect(x:0, y:0, width:frame.width, height:frame.height/2)
         addSubview(numerator)
         
         // Bottom half for denominator
-        denominator.frame = CGRect(x:0, y:self.frame.height/2, width:self.frame.width, height:self.frame.height/2)
+        denominator.frame = CGRect(x:0, y:frame.height/2, width:frame.width, height:frame.height/2)
         addSubview(denominator)
+
+        // Embar covers the whole frame, can be used for non-fractional text or middle bar
+        embar.frame = CGRect(x:0, y:0, width:frame.width, height:frame.height)
+        addSubview(embar)
     }
     
-    func setFraction (top: String, bottom: String)
+    // Function to parse a string formatted as fraction
+    // Valid formats are:
+    // ""       For empty string
+    // "nn"     For number
+    // "nn/nn"  For fraction
+    // Everything else is disregarded
+    func setFraction (str: String)
     {
-        numerator.text = top
-        embar.font = UIFont.init(name:"Symbol", size:16)
-        embar.text = "\u{23AF}\u{23AF}"
-        denominator.text = bottom
+        let components = str.components(separatedBy: "/")
+//        print ("String: \(str), Components: \(components)")
+        
+        switch components.count {
+        case 1:
+            setLabels(top: components[0], bottom: "")
+//            print ("Number or Empty String")
+
+        case 2:
+            if !components[0].isEmpty && !components[1].isEmpty {
+                setLabels(top: components[0], bottom: components[1])
+//                print ("Valid Fraction")
+            }
+            else {
+//                print ("Invalid Format")
+            }
+ 
+        default:
+//            print ("Invalid Format")
+            break
+        }
+    }
+
+    // Show values in labels
+    private func setLabels (top: String, bottom: String)
+    {
+        // Format: "nn/nn" - Show fraction
+        if (top.count > 0 && bottom.count > 0) {
+            numerator.text = top
+            embar.font = UIFont.init(name:"Symbol", size:16)
+            embar.text = "\u{23AF}\u{23AF}"
+            denominator.text = bottom
+        }
+        // Format: "nn" - Show number (or empty)
+        else {
+            numerator.text = ""
+            denominator.text = ""
+            embar.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+            embar.text = top
+        }
     }
 
     // Only override draw() if you perform custom drawing.
--- UnitCircle/ViewController.swift
+++ UnitCircle/ViewController.swift
@@ -11,13 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var qView: UIQuartzView!
-    @IBOutlet weak var fracView: UIFractionView!
+    @IBOutlet weak var valSin: UIFractionView!
+    @IBOutlet weak var valCos: UIFractionView!
+    @IBOutlet weak var valTan: UIFractionView!
+    @IBOutlet weak var valCosec: UIFractionView!
+    @IBOutlet weak var valSec: UIFractionView!
+    @IBOutlet weak var valCot: UIFractionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        fracView.setFraction(top: "2𝜋", bottom: "3")
+        valSin.setFraction(str: "1/2")
+        valCos.setFraction(str: "√3/2")
+        valTan.setFraction(str: "1/√3")
+        
+        valCot.setFraction(str: "√3")
+        valCosec.setFraction(str: "2")
+        valSec.setFraction(str: "2/√3")
+/*
+        fracView.setFraction(str: "")
+        fracView.setFraction(str: "0.866")
+        fracView.setFraction(str: "/")
+        fracView.setFraction(str: "3𝜋")
+        fracView.setFraction(str: "1/2")
+        fracView.setFraction(str: "4/")
+        fracView.setFraction(str: "/4")
+        fracView.setFraction(str: "5///5")
+        fracView.setFraction(str: "///6")
+*/
+//        fracView.setFraction(top: "2𝜋", bottom: "3")
+//        fracView.setFraction(top: "2𝜋", bottom: "")
+//        fracView.setFraction(top: "", bottom: "")
     }
 }
-
