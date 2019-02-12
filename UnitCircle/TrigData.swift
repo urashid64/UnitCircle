c9e1257b9fcedef3c5c7a140aabbd7b4222c41a2
Usman Rashid <usman.rashid@gmail.com>
2019-02-12 12:53:19 -0800

    Moved all trig data handling to the singleton

--- UnitCircle/TrigData.swift
+++ UnitCircle/TrigData.swift
@@ -8,9 +8,23 @@
 
 import Foundation
 
+struct TrigValues {
+    let angle: (String, String)
+    let sin: (String, String)
+    let cos: (String, String)
+    let tan: (String, String)
+    let sec: (String, String)
+    let csc: (String, String)
+    let cot: (String, String)
+}
+
+
 class TrigData {
     static let instance = TrigData()
-    private init(){}
+
+    private init() {
+        initTrigValues()
+    }
     
     // Base axis for each quadrant
     enum axis : CaseIterable {
@@ -43,7 +57,79 @@ class TrigData {
         .pi_by_four:    (.pi/4, 45),
         .pi_by_three:   (.pi/3, 60)
     ]
-
+    
     var currentAxis : axis = .plus_x
     var currentStep : step = .pi_by_six
+    
+    var trigValues: [Int : TrigValues] = [:]
+
+    func initTrigValues () {
+        trigValues[  0] = TrigValues(angle: ("0", "0º"),
+                                     sin:("0", "0.0"), cos:("1", "1.0"), tan:("0", "0.0"), sec:("1", "1.0"), csc:("undef", "undef"), cot:("undef", "undef"))
+        
+        trigValues[ 30] = TrigValues(angle: ("π/6", "30º"),
+                                     sin:("1/2", "0.5"), cos:("√3/2", "0.866"), tan:("1/√3", "0.577"), sec:("2/√3", "1.155"), csc:("2", "2.0"), cot:("√3", "1.732"))
+        
+        trigValues[ 45] = TrigValues(angle: ("π/4", "45º"),
+                                     sin:("√2/2", "0.707"), cos:("√2/2", "0.707"), tan:("1", "1.0"), sec:("√2", "1.414"), csc:("√2", "1.414"), cot:("1", "1.0"))
+        
+        trigValues[ 60] = TrigValues(angle: ("π/3", "60º"),
+                                     sin:("√3/2", "0.866"), cos:("1/2", "0.5"), tan:("√3", "1.732"), sec:("2", "2.0"), csc:("2/√3", "1.155"), cot:("1/√3", "0.577"))
+        
+        trigValues[ 90] = TrigValues(angle: ("π/2", "90º"),
+                                     sin:("1", "1.0"), cos:("0", "0.0"), tan:("undef", "undef"), sec:("undef", "undef"), csc:("1", "1.0"), cot:("0", "0.0"))
+        
+        trigValues[120] = TrigValues(angle: ("2π/3", "120º"),
+                                     sin:("√3/2", "0.866"), cos:("-1/2", "-0.5"), tan:("-√3", "-1.732"), sec:("-2", "-2.0"), csc:("2/√3", "1.155"), cot:("-1/√3", "-0.577"))
+        
+        trigValues[135] = TrigValues(angle: ("3π/4", "135º"),
+                                     sin:("√2/2", "0.707"), cos:("-√2/2", "-0.707"), tan:("-1", "-1.0"), sec:("-√2", "-1.414"), csc:("√2", "1.414"), cot:("-1", "-1.0"))
+        
+        trigValues[150] = TrigValues(angle: ("5π/6", "150º"),
+                                     sin:("1/2", "0.5"), cos:("-√3/2", "-0.866"), tan:("-1/√3", "-0.577"), sec:("-2/√3", "-1.155"), csc:("2", "2.0"), cot:("-√3", "-1.732"))
+        
+        trigValues[180] = TrigValues(angle: ("π", "180º"),
+                                     sin:("0", "0.0"), cos:("-1", "-1.0"), tan:("0", "0.0"), sec:("-1", "-1.0"), csc:("undef", "undef"), cot:("undef", "undef"))
+        
+        trigValues[210] = TrigValues(angle: ("7π/6", "210º"),
+                                     sin:("-1/2", "-0.5"), cos:("-√3/2", "-0.866"), tan:("1/√3", "0.577"), sec:("-2/√3", "-1.155"), csc:("-2", "-2.0"), cot:("√3", "1.732"))
+        
+        trigValues[225] = TrigValues(angle: ("5π/4", "225º"),
+                                     sin:("-√2/2", "-0.707"), cos:("-√2/2", "-0.707"), tan:("1", "1.0"), sec:("-√2", "-1.414"), csc:("-√2", "-1.414"), cot:("1", "1.0"))
+        
+        trigValues[240] = TrigValues(angle: ("4π/3", "240º"),
+                                     sin:("-√3/2", "-0.866"), cos:("-1/2", "-0.5"), tan:("√3", "1.732"), sec:("-2", "-2.0"), csc:("-2/√3", "-1.155"), cot:("1/√3", "0.577"))
+        
+        trigValues[270] = TrigValues(angle: ("3π/2", "270º"),
+                                     sin:("-1", "-1.0"), cos:("0", "0.0"), tan:("undef", "undef"), sec:("undef", "undef"), csc:("-1", "-1.0"), cot:("0", "0.0"))
+        
+        trigValues[300] = TrigValues(angle: ("5π/3", "300º"),
+                                     sin:("-√3/2", "-0.866"), cos:("1/2", "0.5"), tan:("-√3", "-1.732"), sec:("2", "2.0"), csc:("-2/√3", "-1.155"), cot:("-1/√3", "-0.577"))
+        
+        trigValues[315] = TrigValues(angle: ("7π/4", "315º"),
+                                     sin:("-√2/2", "-0.707"), cos:("√2/2", "0.707"), tan:("-1", "-1.0"), sec:("√2", "1.414"), csc:("-√2", "-1.414"), cot:("-1", "-1.0"))
+        
+        trigValues[330] = TrigValues(angle: ("11π/6", "330º"),
+                                     sin:("-1/2", "-0.5"), cos:("-√3/2", "-0.866"), tan:("1/√3", "0.577"), sec:("-2/√3", "-1.155"), csc:("-2", "-2.0"), cot:("√3", "1.732"))
+    }
+    
+    
+    // Move to next Angle
+    func nextAngle () {
+        let nextStepIndex = (step.allCases.index(of: currentStep)! + 1) % step.allCases.count
+        currentStep = step.allCases[nextStepIndex]
+        
+        if nextStepIndex == 0 {
+            let nextAxisIndex = (axis.allCases.index(of: currentAxis)! + 1) % axis.allCases.count
+            currentAxis = axis.allCases[nextAxisIndex]
+        }
+    }
+
+    // Return trig values for the current axis + step
+    func currentTrigValues () -> TrigValues {
+        let axis = axisAngle[currentAxis]!.1
+        let step = stepAngle[currentStep]!.1
+        let tv = trigValues[axis + step]!
+        return tv
+    }
 }
--- UnitCircle/ViewController.swift
+++ UnitCircle/ViewController.swift
@@ -26,24 +26,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var valX: UIFractionView!
     @IBOutlet weak var valY: UIFractionView!
 
-    struct TrigValues {
-        let angle: (String, String)
-        let sin: (String, String)
-        let cos: (String, String)
-        let tan: (String, String)
-        let sec: (String, String)
-        let csc: (String, String)
-        let cot: (String, String)
-    }
-
-    var trigValues: [Int : TrigValues] = [:]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        initTrigValues()
-
         let timesNR = UIFont.init(name: "TimesNewRomanPSMT", size: 22.0)!
         valRadians.setFont(font: timesNR)
         valDegrees.setFont(font: timesNR)
@@ -59,75 +44,22 @@ class ViewController: UIViewController {
     }
 
 
-    func initTrigValues () {
-        trigValues[  0] = TrigValues(angle: ("0", "0º"),
-                                     sin:("0", "0.0"), cos:("1", "1.0"), tan:("0", "0.0"), sec:("1", "1.0"), csc:("undef", "undef"), cot:("undef", "undef"))
-        
-        trigValues[ 30] = TrigValues(angle: ("π/6", "30º"),
-                                     sin:("1/2", "0.5"), cos:("√3/2", "0.866"), tan:("1/√3", "0.577"), sec:("2/√3", "1.155"), csc:("2", "2.0"), cot:("√3", "1.732"))
-        
-        trigValues[ 45] = TrigValues(angle: ("π/4", "45º"),
-                                     sin:("√2/2", "0.707"), cos:("√2/2", "0.707"), tan:("1", "1.0"), sec:("√2", "1.414"), csc:("√2", "1.414"), cot:("1", "1.0"))
-        
-        trigValues[ 60] = TrigValues(angle: ("π/3", "60º"),
-                                     sin:("√3/2", "0.866"), cos:("1/2", "0.5"), tan:("√3", "1.732"), sec:("2", "2.0"), csc:("2/√3", "1.155"), cot:("1/√3", "0.577"))
-        
-        trigValues[ 90] = TrigValues(angle: ("π/2", "90º"),
-                                     sin:("1", "1.0"), cos:("0", "0.0"), tan:("undef", "undef"), sec:("undef", "undef"), csc:("1", "1.0"), cot:("0", "0.0"))
-        
-        trigValues[120] = TrigValues(angle: ("2π/3", "120º"),
-                                     sin:("√3/2", "0.866"), cos:("-1/2", "-0.5"), tan:("-√3", "-1.732"), sec:("-2", "-2.0"), csc:("2/√3", "1.155"), cot:("-1/√3", "-0.577"))
-        
-        trigValues[135] = TrigValues(angle: ("3π/4", "135º"),
-                                     sin:("√2/2", "0.707"), cos:("-√2/2", "-0.707"), tan:("-1", "-1.0"), sec:("-√2", "-1.414"), csc:("√2", "1.414"), cot:("-1", "-1.0"))
-        
-        trigValues[150] = TrigValues(angle: ("5π/6", "150º"),
-                                     sin:("1/2", "0.5"), cos:("-√3/2", "-0.866"), tan:("-1/√3", "-0.577"), sec:("-2/√3", "-1.155"), csc:("2", "2.0"), cot:("-√3", "-1.732"))
-        
-        trigValues[180] = TrigValues(angle: ("π", "180º"),
-                                     sin:("0", "0.0"), cos:("-1", "-1.0"), tan:("0", "0.0"), sec:("-1", "-1.0"), csc:("undef", "undef"), cot:("undef", "undef"))
-        
-        trigValues[210] = TrigValues(angle: ("7π/6", "210º"),
-                                     sin:("-1/2", "-0.5"), cos:("-√3/2", "-0.866"), tan:("1/√3", "0.577"), sec:("-2/√3", "-1.155"), csc:("-2", "-2.0"), cot:("√3", "1.732"))
-        
-        trigValues[225] = TrigValues(angle: ("5π/4", "225º"),
-                                     sin:("-√2/2", "-0.707"), cos:("-√2/2", "-0.707"), tan:("1", "1.0"), sec:("-√2", "-1.414"), csc:("-√2", "-1.414"), cot:("1", "1.0"))
-        
-        trigValues[240] = TrigValues(angle: ("4π/3", "240º"),
-                                     sin:("-√3/2", "-0.866"), cos:("-1/2", "-0.5"), tan:("√3", "1.732"), sec:("-2", "-2.0"), csc:("-2/√3", "-1.155"), cot:("1/√3", "0.577"))
-        
-        trigValues[270] = TrigValues(angle: ("3π/2", "270º"),
-                                     sin:("-1", "-1.0"), cos:("0", "0.0"), tan:("undef", "undef"), sec:("undef", "undef"), csc:("-1", "-1.0"), cot:("0", "0.0"))
-        
-        trigValues[300] = TrigValues(angle: ("5π/3", "300º"),
-                                     sin:("-√3/2", "-0.866"), cos:("1/2", "0.5"), tan:("-√3", "-1.732"), sec:("2", "2.0"), csc:("-2/√3", "-1.155"), cot:("-1/√3", "-0.577"))
-        
-        trigValues[315] = TrigValues(angle: ("7π/4", "315º"),
-                                     sin:("-√2/2", "-0.707"), cos:("√2/2", "0.707"), tan:("-1", "-1.0"), sec:("√2", "1.414"), csc:("-√2", "-1.414"), cot:("-1", "-1.0"))
-        
-        trigValues[330] = TrigValues(angle: ("11π/6", "330º"),
-                                     sin:("-1/2", "-0.5"), cos:("-√3/2", "-0.866"), tan:("1/√3", "0.577"), sec:("-2/√3", "-1.155"), csc:("-2", "-2.0"), cot:("√3", "1.732"))
-    }
-
-
     func updateTrigLabels () {
-        let td = TrigData.instance
-        let axis = td.axisAngle[td.currentAxis]!.1
-        let step = td.stepAngle[td.currentStep]!.1
         
-        valRadians.setFraction(str: trigValues[axis+step]!.angle.0)
-        valDegrees.setFraction(str: trigValues[axis+step]!.angle.1)
+        let tv = TrigData.instance.currentTrigValues()
+        valRadians.setFraction(str: tv.angle.0)
+        valDegrees.setFraction(str: tv.angle.1)
         
-        valSin.setFraction(str: trigValues[axis+step]!.sin.0)
-        valCos.setFraction(str: trigValues[axis+step]!.cos.0)
-        valTan.setFraction(str: trigValues[axis+step]!.tan.0)
+        valSin.setFraction(str: tv.sin.0)
+        valCos.setFraction(str: tv.cos.0)
+        valTan.setFraction(str: tv.tan.0)
         
-        valSec.setFraction(str: trigValues[axis+step]!.sec.0)
-        valCosec.setFraction(str: trigValues[axis+step]!.csc.0)
-        valCot.setFraction(str: trigValues[axis+step]!.cot.0)
+        valSec.setFraction(str: tv.sec.0)
+        valCosec.setFraction(str: tv.csc.0)
+        valCot.setFraction(str: tv.cot.0)
         
-        valX.setFraction(str: trigValues[axis+step]!.cos.0)
-        valY.setFraction(str: trigValues[axis+step]!.sin.0)
+        valX.setFraction(str: tv.cos.0)
+        valY.setFraction(str: tv.sin.0)
 
         // Refresh to draw new highlighted angle
         qView.setNeedsDisplay()
@@ -135,14 +67,7 @@ class ViewController: UIViewController {
 
 
     @objc func handleTap (_: UITapGestureRecognizer) {
-        let td = TrigData.instance
-        let nextStepIndex = (TrigData.step.allCases.index(of: td.currentStep)! + 1) % TrigData.step.allCases.count
-        td.currentStep = TrigData.step.allCases[nextStepIndex]
-
-        if nextStepIndex == 0 {
-            let nextAxisIndex = (TrigData.axis.allCases.index(of: td.currentAxis)! + 1) % TrigData.axis.allCases.count
-            td.currentAxis = TrigData.axis.allCases[nextAxisIndex]
-        }
+        TrigData.instance.nextAngle()
         updateTrigLabels()
     }
 }
